9d61513fd6483d1cf861975e0bfbf5ed9f2f9218
Samuel Goma <sgomag@gmail.com>
2026-03-30 15:58:17 +0200

    added try/catch

--- day-26-NATO-phonetic-alphabet/main.py
+++ day-26-NATO-phonetic-alphabet/main.py
@@ -1,10 +1,20 @@
 import pandas
 
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
-nato_dic = {row.letter:row.code for (index, row) in nato_df.iterrows()}
+nato_dict = {row.letter:row.code for (index, row) in nato_df.iterrows()}
+
+
+def spell(word):
+    try:
+        nato_word = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        return False
+    else:
+        print(nato_word)
+
 
 word = input("What is the word?: ").upper()
-print(word)
-nato_word = [nato_dic.get(letter) for letter in word]
 
-print(nato_word)
+while spell(word) is False:
+    word = input("What is the word?: ").upper()
